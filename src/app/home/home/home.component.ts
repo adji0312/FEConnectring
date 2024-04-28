aac0f58ca08f210ed4651fde4842091dc4676aba
adji0312 <adjibudhi77@gmail.com>
2024-04-29 01:27:19 +0700

    baru selesai fix

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { Merchant } from 'src/app/user/merchant/merchant.model';
 import { MerchantService } from 'src/app/user/merchant/merchant.service';
 
@@ -16,9 +17,10 @@ export class HomeComponent implements OnInit {
   merchantList!: Merchant[];
 
   constructor(
-              private http: HttpClient,
-              private merchantService: MerchantService,
-              private sanitizer: DomSanitizer,
+    private http: HttpClient,
+    private merchantService: MerchantService,
+    private sanitizer: DomSanitizer,
+    private router: Router
             ) {
     this.http = http;
     this.loginuser = JSON.parse(localStorage.getItem('currentUser') as string);
@@ -37,5 +39,9 @@ export class HomeComponent implements OnInit {
     return this.sanitizer.bypassSecurityTrustUrl(objectURL);
   }
 
-
+  onOpenCatering(merchant: Merchant): void{
+    console.log(merchant);
+    this.merchantService.viewCatering = merchant;
+    this.router.navigate(['/detailCatering']);
+  }
 }
